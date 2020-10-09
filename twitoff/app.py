f03b457347d8d904be52c78f364909bfe437bb85
term1830 <term1830@gmail.com>
2020-10-09 18:29:28 -0400

    reset method

--- twitoff/app.py
+++ twitoff/app.py
@@ -52,6 +52,7 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
+        return render_template('base.html', title='Reset Database!', users=User.query.all())
 
     @app.route('/update',methods=['GET'])
     def update():
