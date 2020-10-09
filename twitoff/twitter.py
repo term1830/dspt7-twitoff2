1eda63dc00ac5d07e3d52ae08a48748480f5fa87
term1830 <term1830@gmail.com>
2020-10-09 19:39:08 -0400

    spacy

--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -14,7 +14,7 @@ TWITTER_AUTH.set_access_token(getenv('TWITTER_ACCESS_TOKEN'),
 TWITTER = tweepy.API(TWITTER_AUTH)
 
 # Load SpaCy pre-trained model
-nlp = spacy.load('spacy_md_model')
+nlp = spacy.load('spacy_sm_model')
 
 
 def vectorize_tweet(nlp, tweet_text):
